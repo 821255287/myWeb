20ee20f5190ee23d423135d4fed3c499e11e8142
linzhili <57003937+821255287@users.noreply.github.com>
2024-10-15 23:18:10 +0800

    Add files via upload

---
+++ static/js/language.js
@@ -0,0 +1,28 @@
+// let langList =,
+
+export default {
+	home: {
+		ch: "首页",
+		en: "Home"
+	},
+	contact: {
+		ch: "联系",
+		en: "contact"
+	},
+	serve: {
+		ch: "服务",
+		en: "serve"
+	},
+	mine: {
+		ch: "我的",
+		en: "mine"
+	},
+	title: {
+		ch: "标题",
+		en: "title"
+	},
+	con: {
+		ch: "内容",
+		en: "content"
+	}
+}
